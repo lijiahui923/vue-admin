884d0ce1087ce1b096893b134a4ac9242d46e4b4
lijiahui <2836755307@qq.com>
2020-05-15 17:55:58 +0800

    登录页面添加切换的样式

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="login">
-    <el-button>默认按钮</el-button>
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li v-for="item in menuTab" :key="item.id" :class="{'current': item.current}" @click="toggleMenu(item)">{{item.text}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -15,18 +19,48 @@ export default {
   props: {},
   components: {},
   data() {
-    return {};
+    return {
+      menuTab: [
+        { id: 1, text: '登录', current: true },
+        { id: 2, text: '注册', current: false }
+      ]
+    };
   },
   computed: {},
   watch: {},
   mounted() {},
   created() {},
-  methods: {}
+  methods: {
+    toggleMenu (data) {
+      this.menuTab.forEach(elem => {
+        elem.current = false;
+      });
+      data.current = true;
+    }
+  }
 };
 </script>
 <style scoped lang="scss">
-#login{
+#login {
   height: 100vh;
   background-color: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  color: #fff;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    border-radius: 2px;
+  }
+  .current {
+    background-color: rgba(0,0,0,.1);
+  }
 }
 </style>
